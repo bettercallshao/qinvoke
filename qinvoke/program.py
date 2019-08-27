5213d6edb3f9ba7cf6ddacd1e2c9bbfd1e7deb99
Shaoqing Tan <tansq7@gmail.com>
2019-08-26 23:52:11 -0400

    Bug fixes and support python2.7

--- qinvoke/program.py
+++ qinvoke/program.py
@@ -1,9 +1,10 @@
+import contextlib
 import inspect
 import io
 import os
 import re
+import sys
 import webbrowser
-from contextlib import redirect_stdout
 
 from flask import Flask, render_template, request
 from invoke import Program as Base
@@ -11,8 +12,17 @@ from invoke import Program as Base
 ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
 
 
+@contextlib.contextmanager
+def redirect_stdout():
+    original = sys.stdout
+    sys.stdout = io.StringIO()
+    yield sys.stdout
+    sys.stdout = original
+
+
 class Program(Base):
     def serve(self):
+        self.create_config()
         self.parse_core(None)
         self.parse_collection()
         self.parse_tasks()
@@ -30,17 +40,21 @@ class Program(Base):
         ]
         tasks_map = {t['name']: t for t in tasks}
 
-        app = Flask('qinvoke', root_path=os.path.dirname(__file__))
+        with redirect_stdout() as f:
+            self.print_version()
+            title = f.getvalue().strip()
+        app = Flask(title, root_path=os.path.dirname(__file__))
 
         @app.route('/')
         def home():
-            return render_template('home.html', tasks=tasks)
+            return render_template('home.html', title=title, tasks=tasks)
 
         @app.route('/<task>')
         def task(task):
             try:
                 return render_template(
                     'task.html',
+                    title=title,
                     task=tasks_map[task],
                     out='')
             except KeyError:
@@ -77,19 +91,19 @@ class Program(Base):
                     for word in _list_from_arg(arg)
                 ]
 
-                f = io.StringIO()
-                with redirect_stdout(f):
+                with redirect_stdout() as f:
                     self.run(argv)
-                return render_template(
-                    'task.html',
-                    task=task_dict,
-                    cmd=' '.join(argv),
-                    out=ansi_escape.sub('', f.getvalue())
-                )
+                    return render_template(
+                        'task.html',
+                        title=title,
+                        task=task_dict,
+                        cmd=' '.join(argv),
+                        out=ansi_escape.sub('', f.getvalue())
+                    )
 
             except KeyError:
                 return 400, 'no such task {}!'.format(task)
 
         port = os.getenv('PORT', '8800')
         webbrowser.open_new_tab('http://localhost:{}'.format(port))
-        app.run(port=port)
+        app.run(host='0.0.0.0', port=port)
